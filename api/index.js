ee89fed5e504868e7cee5f0454d5f5c34887bd84
Blessed Tabvirwa <blessedndoro2014@gmail.com>
2023-11-09 22:59:38 +1100

    core: connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
-import { log } from 'console';
-import express from 'express';
+import { log } from "console";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
