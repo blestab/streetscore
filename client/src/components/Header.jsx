b3c97f87767b8cd8131fd9adb3ace71831ca55ca
Blessed Tabvirwa <blessedndoro2014@gmail.com>
2023-11-09 22:29:06 +1100

    core: create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,25 +17,27 @@ export default function Header() {
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-slate-600" />
-          <ul className="flex gap-4">
-            <Link to="/">
-              <li className="hidden sm:inline text-slate-700 hover:underline">
-                Home
-              </li>
-            </Link>
-            <Link to="/about">
-              <li className="hidden sm:inline text-slate-700 hover:underline">
-                About
-              </li>
-            </Link>
-            <Link to="/sign-in">
-              <li className="hidden sm:inline text-slate-700 hover:underline">
-                Sign In
-              </li>
-            </Link>
-          </ul>
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Sign In
+            </li>
+          </Link>
+        </ul>
       </div>
     </header>
   );
