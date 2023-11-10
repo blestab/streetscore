451d8f5fde356dbb131db2fa1b6aee570b418c60
Blessed Tabvirwa <blessedndoro2014@gmail.com>
2023-11-10 18:19:52 +1100

    fix (api): error handler add success:false and status code

--- api/index.js
+++ api/index.js
@@ -30,5 +30,9 @@ app.use("/api/auth", authRoute);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
-  res.status(statusCode).json({ message });
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 }); // Error handler
